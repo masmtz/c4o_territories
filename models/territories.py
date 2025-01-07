833853b9d5ff4812e5b70d973ff2a0d02ab3792a
masmtz <s.mtz.casillas@gmail.com>
2025-01-06 17:38:32 -0700

    [ADD] fields parameters

--- models/territories.py
+++ models/territories.py
@@ -25,12 +25,23 @@ class TerritoryStreet(models.Model):
     sidewalk = fields.Selection(
         [("n", "North"), ("s", "South"), ("e", "East"), ("w", "West")]
     )
-    n_street = fields.Char(default="N/A")
-    s_street = fields.Char(default="N/A")
-    e_street = fields.Char(default="N/A")
-    w_street = fields.Char(default="N/A")
+    n_street = fields.Char()
+    s_street = fields.Char()
+    e_street = fields.Char()
+    w_street = fields.Char()
+    between_streets = fields.Char(compute="compute_between_streets")
     num_houses = fields.Integer()
     notes = fields.Text()
+
+    @api.depends("n_street", "s_street", "w_street", "e_street")
+    def compute_between_streets(self):
+        str_bstreets = ""
+        if self.w_street and self.e_street:
+            str_bstreets = _("Between " + self.w_street + " and " + self.e_street)
+
+        if self.n_street and self.s_street:
+            str_bstreets = _("Between " + self.n_street + " and " + self.s_street)
+        self.between_streets = str_bstreets
 
 
 class TerritoryConfigParamenter(models.Model):
